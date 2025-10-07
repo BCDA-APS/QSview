c8bc81897f983adaa141c791a09f4feda2f80c6e
Fanny Rodolakis <rodolakis@anl.gov>
2025-10-07 09:41:20 -0500

    Fix LICENSE file path

--- src/QSviz/licensedialog.py
+++ src/QSviz/licensedialog.py
@@ -25,7 +25,7 @@ class LicenseDialog(QtWidgets.QDialog):
         super().__init__(parent)
         utils.myLoadUi(self.ui_file, baseinstance=self)
 
-        LICENSE_FILE = pathlib.Path(__file__).parent / "../LICENSE.txt"
+        LICENSE_FILE = pathlib.Path(__file__).parent / "../../LICENSE.txt"
         self.setModal(True)
         license_text = open(LICENSE_FILE, "r").read()
         self.license.setText(license_text)
