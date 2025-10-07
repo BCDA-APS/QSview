0a4671f840f67f869afdd3ee6eb7d84c3d03a8a0
Fanny Rodolakis <rodolakis@anl.gov>
2025-10-07 13:10:57 -0500

    Add RE status, tentative

--- src/QSviz/widgets/status.py
+++ src/QSviz/widgets/status.py
@@ -15,9 +15,17 @@ class StatusWidget(QtWidgets.QWidget):
     def __init__(self, parent=None, rm_api=None):
         super().__init__(parent)
         utils.myLoadUi(self.ui_file, baseinstance=self)
+        self.rm_api = rm_api
         self.setup()
+        self.update_rm_status()
 
     def setup(self):
         """Connect signals and slots."""
         # TODO: Add signal/slot connections here
         pass
+
+    def update_rm_status(self):
+        """Update the status of the REManager API."""
+        status = self.rm_api.status()
+        RE_state = status.get("re_state", "None")
+        self.runengineLabel.setText(RE_state)
