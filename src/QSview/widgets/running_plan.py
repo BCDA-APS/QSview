fbe80ed32dd0dcb6e056227d96320bab38eeedb9
Fanny Rodolakis <rodolakis@anl.gov>
2025-10-22 10:11:45 -0500

    Clean up unused methods in running_plan.py

--- src/QSview/widgets/running_plan.py
+++ src/QSview/widgets/running_plan.py
@@ -15,18 +15,7 @@ class RunningPlanWidget(QtWidgets.QWidget):
     def __init__(self, parent=None, model=None):
         super().__init__(parent)
         utils.myLoadUi(self.ui_file, baseinstance=self)
-        self.setup()
         self.model = model
-
-    def setup(self):
-        """Connect signals and slots."""
-        # TODO: Add signal/slot connections here
-        pass
-
-    def onConnectionChanged(self, is_connected, control_addr, info_addr):
-        """Handle connection changes from QueueServerModel signal."""
-        # TODO: Add connection-dependent updates here
-        pass
 
     def onStatusChanged(self, is_connected, status):
         """Handle periodic status updates from model (every 1s)."""
